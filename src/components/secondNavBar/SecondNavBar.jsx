0e358fd65dc3dda200de9aeab9936414ed636e41
michelleordelia <michelleordelia@gmail.com>
2023-07-19 10:18:23 +0800

    second navbar modified-2

--- src/components/secondNavBar/SecondNavBar.jsx
+++ src/components/secondNavBar/SecondNavBar.jsx
@@ -6,10 +6,10 @@ export const SecondNavBar = () => {
     <div>
         <div className = "navbar">
             <p style={{paddingLeft: "9%"}}><a href = "#home">Home</a></p>
-            <p><a href = "#upload">Upload</a></p>
-            <p><a href = "#invoices">Invoices</a></p>
-            <p><a href = "#soa">Statement of Account</a></p>
-            <p><a href = "#product">Product</a></p>
+            <p><a className="upload-navbar" href = "#upload">Upload</a></p>
+            <p><a className= "invoices-navbar" href = "#invoices">Invoices</a></p>
+            <p><a className="soa-navbar" href = "#soa">Statement of Account</a></p>
+            <p><a className="product-navbar" href = "#product">Product</a></p>
 
             <div style={{paddingRight: "9%"}} className = "logout">
               <a href="#signin">
