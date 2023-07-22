793d55f702acfb7cd7a2e6eb1b17cb9df39a3448
yupitsmeeric <muhammadilhamfadhilah@gmail.com>
2023-07-23 07:57:58 +0800

    added registration and signin tests

--- user_auth.test.js
+++ user_auth.test.js
@@ -23,6 +23,8 @@ beforeAll(async () => {
       });
 });
 
+
+// Basic connection test
 test("Gets the root endpoint", async () => {
   // Sends GET Request to / endpoint
   const res = await request.get("/");
@@ -31,7 +33,61 @@ test("Gets the root endpoint", async () => {
   // ...
 }, 8000);
 
+// Test user creatoion; make a user whos email is the current datetime
+var myemail
+test('Make new user', async () => {
+    myemail = new Date().getTime() + '@test.com.sg'
+    const res = await request.post("/register").send({
+        email:myemail,
+        password:"password",
+        company:"ichigo",
+        name:"ruby"
+    });
+    console.log(res);
+    expect(res.status).toEqual(201);
 
+});
+// and maybe delete this user later
+// Test signing up with duplicate email: use the same email used as above
+test('Make new user with duplicate email', async () => {
+    const res = await request.post("/register").send({
+        email:myemail,
+        password:"password",
+        company:"ichigo",
+        name:"ruby"
+    });
+    expect(res.status).toEqual(500); // should return an error
+
+}); 
+
+/// Test signin
+// Signin successful
+test('Successful signin', async () => {
+    const res = await request.post("/signin").send({
+        email:myemail,
+        password:"password"
+    });
+    expect(res.status).toEqual(200);
+    expect(Boolean(res.body.token)).toEqual(true); // check that a random token is generated
+});
+
+// email does not exist
+test('No such email found', async() => {
+    const res = await request.post("/signin").send({
+        email:"ThisEmailDoesNotExist@test.com",
+        password:"password"
+    });
+    expect(res.status).toEqual(404);
+});
+    
+// password does not match
+test('No such email found', async() => {
+    const res = await request.post("/signin").send({
+        email:myemail,
+        password:"passwordddddd"
+    });
+    expect(res.status).toEqual(400);
+});
 
 // close the mongoose connection
 afterAll(async () => {
