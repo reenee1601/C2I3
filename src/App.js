de5957b95d1b6129d89dda5c25b7b7951165759a
leowsimin <leowsmr@gmail.com>
2023-07-22 11:29:26 +0800

    Packages, App, Index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { RegisterPage, HomePage } from './pages';
+import { RegisterPage, HomePage, Signin, UploadPage, EditDocumentPage } from './pages';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';;
 
 const App = () => {
   return (
@@ -11,6 +13,10 @@ const App = () => {
         <Routes>
           <Route index element = {<RegisterPage />} />
           <Route path = '/registerpage' element = {<RegisterPage />} />
+          <Route path = '/signinpage' element = {<Signin />} />
+          <Route path = '/homepage' element = {<HomePage />} />
+          <Route path = '/uploadpage' element = {<UploadPage />} />
+          <Route path = '/editdocumentpage' element = {<EditDocumentPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 export { default as RegisterPage } from './registerPage/RegisterPage';
 export { default as HomePage } from './homePage/HomePage';
-export { default as SoaPage } from './soaPage/SoaPage';
-export { default as DetailedSoaPage } from './detailedSoaPage/DetailedSoaPage';
+export { default as Signin } from './signInPage/Signin';
+export { default as UploadPage } from './uploadPage/UploadPage';
+export { default as EditDocumentPage } from './editDocumentPage/EditDocumentPage';
+// export { default as SoaPage } from './soaPage/SoaPage';
+// export { default as DetailedSoaPage } from './detailedSoaPage/DetailedSoaPage';
