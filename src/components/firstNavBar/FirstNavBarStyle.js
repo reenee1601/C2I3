1542e7bbabb10558713f6a0100e2f2452c05381d
leowsimin <leowsmr@gmail.com>
2023-08-02 03:39:34 +0800

    FirstNavBar + SecondNavBar + SubmitPopUp Commit

---
+++ src/components/firstNavBar/FirstNavBarStyle.js
@@ -0,0 +1,21 @@
+export const signin_register_navbar = {
+    display: "inline-flex",
+    flexDirection: "row",
+}
+
+export const linkstyles = {
+    color: "#3A3A3A",
+    textAlign: "center",
+    fontFamily: "Inter",
+    fontSize: "20px",
+    fontStyle: "normal",
+    fontWeight: "800",
+    lineHeight: "normal",
+    textDecoration: "none",
+    
+    padding: "10px 20px",
+    borderRadius: "10px",
+
+    marginRight: "20px",
+    marginLeft: "20px",
+  }
--- src/components/secondNavBar/SecondNavBar.jsx
+++ src/components/secondNavBar/SecondNavBar.jsx
@@ -1,128 +1,65 @@
 import React, { useState } from 'react';
 
-import './secondNavBar.css';
-import { IoLogOutOutline } from "react-icons/io5";
+import { IoLogOut } from "react-icons/io5";
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
+import {
+  linkstyles,
+  navbar,
+  logouticon,
+  navbarContainer,
+  logoutStyle
+} from "./SecondNavBarStyle"
 
 export const SecondNavBar = () => {
+
+  const location = useLocation();
+
   const [linkState, setLinkState] = useState([
-    { label: 'Home', active: false, hovered: false, route: "homepage"},
-    { label: 'Upload', active: false, hovered: false, route: "uploadpage" },
-    { label: 'Invoice', active: false, hovered: false, route: "invoicepage"},
-    { label: 'Statement Of Account', active: false, hovered: false, route: "soapage" },
-    { label: 'Product', active: false, hovered: false, route:"productpage" },
+    { label: 'HOME', active: false, hovered: false, route: "homepage"},
+    { label: 'UPLOAD', active: false, hovered: false, route: "uploadpage" },
+    { label: 'INVOICE', active: false, hovered: false, route: "invoicepage"},
+    { label: 'STATEMENT OF ACCOUNT', active: false, hovered: false, route: "soapage" },
+    { label: 'PRODUCT', active: false, hovered: false, route:"productpage" },
   ]);
 
-  // Function to handle onMouseOver
-  function handleMouseOver(index) {
-    setLinkState((prevLinkState) =>
-      prevLinkState.map((link, i) =>
-        i === index ? { ...link, hovered: true } : { ...link, hovered: false }
-      )
-    );
-  }
-
-  // Function to handle onMouseLeave
-  function handleMouseLeave(index) {
-    setLinkState((prevLinkState) =>
-      prevLinkState.map((link, i) =>
-        i === index ? { ...link, hovered: false } : link
-      )
-    );
-  }
-
-  // Function to handle onClick
-  function handleLinkClick(index) {
-    setLinkState((prevLinkState) =>
-      prevLinkState.map((link, i) =>
-        i === index ? { ...link, active: !link.active } : { ...link, active: false }
-      )
-    );
-    console.log('Button Clicked');
-  }
-
-  const linkstyles = {
-    color: "#535353",
-    fontSize: "15px",
-    fontStyle: "normal",
-    fontWeight: "700",
-    fontFamily: "Inter",
-    textDecoration: "none",
-    
-    backgroundColor: "transparent",
-    borderBottom: "2px solid transparent",
-  }
-
-  const navbar = {
-    display:"inline-flex",
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    width: "100%",
-    margin: "4% 2% 2% 4%",
-  }
-
-  const logout = {
-    display: "flex",
-    flexDirection: "row",
-
-    alignItems: "center",
-    justifyContent: "center",
-    paddingRight: "9%",
-    }
-
-  const logouticon = {
-    width: "40px",
-    height: "40px",
-    color: "#535353",
-
-    marginRight:"10px"
-  }
-
   return (
-    <div>
+    <div style={navbarContainer}>
       <div style={navbar}>
         {linkState.map((link, index) => (
           <p key={index}>
-            <Link to={`/${link.route.toLowerCase()}`}
-              onClick={() => handleLinkClick(index)}
-              onMouseOver={() => handleMouseOver(index)}
-              onMouseLeave={() => handleMouseLeave(index)}
+            <Link
+              to={`/${link.route.toLowerCase()}`}
               style={{
                 ...linkstyles,
-                color: link.active
-                  ? '#418EFF' // Active background color
-                  : link.hovered
-                  ? '#418EFF' // Hovered background color
-                  : '#535353', // Default background color
-
-                  // link.active not working.
-                // borderBottom: link.active 
-                //   ? "2px solid #418EFF" 
-                //   : "2px solid transparent",
-
-                borderBottom: link.hovered 
-                ? "2px solid #418EFF" 
-                : "2px solid transparent",
+                backgroundColor: location.pathname.includes(link.route.toLowerCase())
+                  ? link.route.toLowerCase() === "homepage"
+                    ? "#EEFFE8"
+                    : link.route.toLowerCase() === "uploadpage"
+                    ? "#E8F4FF"
+                    : link.route.toLowerCase() === "invoicepage"
+                    ? "#FFE8E8"
+                    : link.route.toLowerCase() === "soapage"
+                    ? "#FFF6E8"
+                    : link.route.toLowerCase() === "productpage"
+                    ? "#F2E8FF"
+                    : "#E8F4FF"
+                  : "transparent", // Default background color
               }}
             >
               {link.label}
             </Link>
           </p>
         ))}
-
-        <div style = {logout}>
-          <a href="/signinpage">
-          <IoLogOutOutline style = {logouticon}/>
-          </a>
-          <p>
-            <a style = {linkstyles} href="/signinpage">
-              Log Out
-            </a>
-          </p>
-        </div>
-
       </div>
+
+      <div style={logoutStyle}>
+        <a href="/signinpage">
+          <IoLogOut style = {logouticon}/>
+        </a>
+      </div>
+
     </div>
   );
 }
