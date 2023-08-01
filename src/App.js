f177a18d5d057520e3070c5c89d6ef6959856858
michelleordelia <michelleordelia@gmail.com>
2023-08-02 03:52:54 +0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { RegisterPage, Signinpage, HomePage, DetailedInvoicePage, DetailedSoaPage, EditDocumentPage, 
-          InvoicePage, SoaPage, UploadPage, CompareDocumentPage } from './pages';
+          InvoicePage, SoaPage, UploadPage, CompareDocumentPage, ProductPage } from './pages';
 
-import './App.css';
 
 const App = () => {
-  const [selected_amount, setSelected_amount] = useState("");
-  const [selected_date, setSelected_date] = useState("");
-  const [selected_invoicenum, setSelected_invoicenum] = useState("");
-  const [selected_suppliername, setSelected_suppliername] = useState("");
   return (
     <div>
       <BrowserRouter>
@@ -23,11 +18,8 @@ const App = () => {
           <Route path = '/detailedsoapage' element = {<DetailedSoaPage />} />
           <Route path = '/editdocumentpage' element = {<EditDocumentPage />} />
           <Route path = '/invoicepage' element = {<InvoicePage />} />
-          <Route path = '/soapage' element = {<SoaPage 
-            selected_amount={selected_amount} setSelected_amount={setSelected_amount}
-            selected_date={selected_date} setSelected_date={setSelected_date} 
-            selected_invoicenum={selected_invoicenum} setSelected_invoicenum={setSelected_invoicenum}
-            selected_suppliername={selected_suppliername} setSelected_suppliername={setSelected_suppliername}/>} />
+          <Route path = '/soapage' element = {<SoaPage />}/>
+          <Route path = '/productpage' element = {<ProductPage />}/>
           <Route path = '/uploadpage' element = {<UploadPage />} />
           <Route path = '/comparedocumentpage' element = {<CompareDocumentPage />} />
         </Routes>
