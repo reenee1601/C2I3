951ae2dac5681d860614e8f3e457f778f80a80d1
ShelenGo <shelengo21@gmail.com>
2023-08-02 23:41:45 +0800

    updated user unit testing

--- backend/test/userAuth.test.js
+++ backend/test/userAuth.test.js
@@ -1,16 +1,17 @@
 process.env['NODE_DEV'] = 'TEST'
 
-const app = require("./server.js"); // Link to your server file
+const app = require("../app.js"); 
 const supertest = require("supertest");
 const request = supertest(app);
-const dbConfig = require("./config/db.config");
-const db = require("./models");
+const mongoose = require('mongoose')
+const url = 'mongodb+srv://reenee1601:QNbeHPQLj8pwP7UC@cluster0.i4ee9cb.mongodb.net/project_data?retryWrites=true&w=majority';
+
 
 beforeAll(async () => {
     //connection to mongoDB database
 
-    await db.mongoose
-      .connect(dbConfig.URL,{
+    await mongoose
+      .connect(url,{
         useNewUrlParser: true,
         useUnifiedTopology: true
       })
@@ -24,20 +25,11 @@ beforeAll(async () => {
 });
 
 
-// Basic connection test
-test("Gets the root endpoint", async () => {
-  // Sends GET Request to / endpoint
-  const res = await request.get("/");
-  //console.log(res);
-  expect(res.body.message).toEqual("Welcome to login/registration page.")
-  // ...
-}, 8000);
-
 // Test user creatoion; make a user whos email is the current datetime
 var myemail
 test('Make new user', async () => {
     myemail = new Date().getTime() + '@test.com.sg'
-    const res = await request.post("/register").send({
+    const res = await request.post("/users/register").send({
         email:myemail,
         password:"password",
         company:"ichigo",
@@ -50,20 +42,20 @@ test('Make new user', async () => {
 // and maybe delete this user later
 // Test signing up with duplicate email: use the same email used as above
 test('Make new user with duplicate email', async () => {
-    const res = await request.post("/register").send({
+    const res = await request.post("/users/register").send({
         email:myemail,
         password:"password",
         company:"ichigo",
         name:"ruby"
     });
-    expect(res.status).toEqual(500); // should return an error
+    expect(res.status).toEqual(400); 
 
 }); 
 
 /// Test signin
 // Signin successful
 test('Successful signin', async () => {
-    const res = await request.post("/signin").send({
+    const res = await request.post("/users/signin").send({
         email:myemail,
         password:"password"
     });
@@ -73,7 +65,7 @@ test('Successful signin', async () => {
 
 // email does not exist
 test('No such email found', async() => {
-    const res = await request.post("/signin").send({
+    const res = await request.post("/users/signin").send({
         email:"ThisEmailDoesNotExist@test.com",
         password:"password"
     });
@@ -82,7 +74,7 @@ test('No such email found', async() => {
     
 // password does not match
 test('No such email found', async() => {
-    const res = await request.post("/signin").send({
+    const res = await request.post("/users/signin").send({
         email:myemail,
         password:"passwordddddd"
     });
@@ -91,7 +83,7 @@ test('No such email found', async() => {
 
 // close the mongoose connection
 afterAll(async () => {
-    await db.mongoose.connection.close();
+    await mongoose.connection.close();
     //await request.post('/close-mongoose-connection');
     console.log('connection closed');
 });
