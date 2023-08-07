83b802ff76fcfc686dc8d6f411ef8f0645536f8a
rybyte <67882888+rybyte@users.noreply.github.com>
2023-08-07 20:47:26 +0800

    front end component edit

---
+++ frontend/src/components/scanSuccessfully/ScanSuccessfullyStyle.js
@@ -0,0 +1,41 @@
+export const scanSuccessfullyContainer = {
+    position: "fixed", // Change this to "fixed" to center the pop-up in the viewport
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 9999,
+    background: "rgba(0, 0, 0, 0.5)", // semi-transparent background to create overlay effect
+  
+    // Your other styles...
+  };
+
+export const documentSuccessfullyScannedText = {
+    color: "#FFF",
+    textAlign: "center",
+    fontFamily: "Inter",
+    fontSize: "32px",
+    fontStyle: "normal",
+    fontWeight: "800",
+    lineHeight: "normal",
+}
+
+export const documentSucccessfullyScannedText2 = {
+    color: "#FFF",
+    textAlign: "center",
+    fontFamily: "Inter",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: "400",
+    lineHeight: "normal",
+}
+
+export const documentScanSuccessfullyContainer = {
+    display: "flex",
+    flexDirection: "column",
+}
+
---
+++ frontend/src/components/soaQueryBar/SoaQueryBarStyle.js
@@ -0,0 +1,142 @@
+export const queryPopUp = {
+    zIndex: "9999",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+
+    background: "rgba(0, 0, 0, 0.4)",
+ }
+
+ export const queryPopUpInner = {
+    position: "relative",
+    width: "30%",
+    height:"100%",
+    marginLeft: "0",
+
+    background: "#FFFFFF",
+ }
+
+ export const goBackStyle = {
+    marginLeft: "90%",
+    marginTop: "1%",
+    border: "none",
+    background: "transparent",
+ }
+
+ export const goBackIconStyle = {
+    width: "20px",
+    height: "20px",
+ }
+
+ export const searchFilterStyle = {
+    display: "flex",
+    flexDirection: "column",
+ }
+
+ export const searchFilterTextStyle = {
+    color: "#3A3A3A",
+    fontFamily: "Inter",
+    fontSize: "20px",
+    fontStyle: "normal",
+    fontWeight: "800",
+    lineHeight: "normal",
+
+    textAlign: "center",
+    marginTop: "10%",
+ }
+
+ export const inputBoxStyle = {
+    display: "flex",
+    flexDirection: "column",
+ }
+
+ export const inputSpanStyle = {
+    color: "#3A3A3A",
+    fontFamily: "Inter",
+    fontSize: "15px",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: "normal",
+
+    marginLeft: "9%",
+    marginTop: "5%",
+ }
+
+ export const userInputStyle = {
+    borderRadius: "5px",
+    background: "rgba(255, 246, 232)",
+    border: "none",
+    width: "40%",
+
+    padding: "2% 2% 2% 2%",
+    marginTop:"2%",
+    marginRight: "2%",
+    marginLeft: "2%",
+ }
+
+ export const allUserInputStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+
+    fontFamily: "Inter",
+    fontSize: "13px",
+ }
+
+ export const userLongerInputStyle = {
+    borderRadius: "5px",
+    background: "rgba(255, 246, 232)",
+    border: "none",
+    width: "85%",
+
+    padding: "2% 2% 2% 2%",
+    marginTop:"2%",
+    marginRight: "2%",
+    marginLeft: "2%",
+ }
+
+ export const resetButtonStyle = {
+    borderRadius: "5px",
+    background: "#3A3A3A",
+    width: "20%",
+    height: "35px",
+    border: "none",
+
+    color: "#FFF",
+    fontFamily: "Inter",
+    fontSize: "15px",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: "normal",
+
+    marginLeft: "2%",
+    marginRight: "2%"
+ }
+
+ export const applyButtonStyle = {
+    borderRadius: "5px",
+    background: "#D3BB96",
+    width: "20%",
+    height: "35px",
+    border: "none",
+
+    color: "#FFF",
+    fontFamily: "Inter",
+    fontSize: "15px",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: "normal",
+
+    marginLeft: "2%",
+    marginRight: "2%"
+ }
+
+ export const queryButtonStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+
+    marginTop: "10%",
+ }
