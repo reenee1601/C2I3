1338bdee85a3418405e61cf7c3a058803f70dab9
Harini Parthasarathy <87818138+reenee1601@users.noreply.github.com>
2023-08-10 03:23:13 +0800

    password update

---
+++ frontend/src/pages/forgotpasswordpage/forgotpasswordpagestyle.js
@@ -0,0 +1,90 @@
+export const resetPasswordContainerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+    minHeight: "100vh", 
+}
+
+export const resetFormStyle = {
+    color: "#474747",
+    fontFamily: "Inter",
+    fontSize: "30px",
+    fontStyle: "normal",
+    fontWeight: "800",
+    lineHeight: "normal",
+
+    marginBottom: "5%",
+}
+
+export const resetPasswordFormContainer = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    width: "50%",
+    height: "100%",
+    borderRadius: "10px",
+    marginTop:"10px",
+
+    width: "50%",
+    height: "100%",
+    margin: '0 auto',
+}
+
+export const forgetPasswordText = {
+    color: "#474747",
+    fontFamily: "Inter",
+    fontSize: "25px",
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "normal",
+    marginTop: "4%",
+}
+
+export const keyNewPasswordText = {
+    color: "#474747",
+    fontFamily: "Inter",
+    fontSize: "12px",
+    fontStyle: "normal",
+    fontWeight: "400",
+    lineHeight: "normal",
+
+    margin: "10px 10px 20px 10px"
+}
+
+export const forgotPasswordInputStyle = {
+
+    borderRadius: "5px",
+    background: "rgba(170, 170, 170, 0.30)",
+    border: "none",
+
+    width: "80%",
+    marginLeft: "10%",
+    marginBottom: "3%",
+    alignItems: "center", 
+
+    paddingTop: "10px",
+    paddingBottom: "10px",
+
+    paddingLeft: "10px",
+}
+
+export const forgetPasswordButtonStyle = {
+    color: "#FFF",
+    fontFamily: "Inter",
+    fontSize: "18px",
+    fontStyle: "normal",
+    fontWeight: "700",
+    lineHeight: "normal",
+
+    borderRadius: "5px",
+    background: "#3A3A3A",
+
+    width: "70%",
+    marginBottom: "5%",
+    alignItems: "center",
+    paddingTop: "10px",
+    paddingBottom: "10px",
+}
